90ed0dfda799f480660b34038d5381f60ed6b3f6
ArlindXh <xhakoliarlind@gmail.com>
2021-10-19 17:45:03 +0200

    include number of likes when checking self info

--- src/controllers/user/self-info.ts
+++ src/controllers/user/self-info.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from 'express';
 import User, { UserInterface } from '../../models/Users';
+import UserLikes, { UserLikesInterface } from '../../models/UserLikes';
 import { verifyAndDecode } from "../../util/jwt";
 import { JwtPayload } from "jsonwebtoken"
 import logger from '../../logger';
@@ -7,7 +8,13 @@ import logger from '../../logger';
 const selfInfo: RequestHandler = async (req: any, res) => {
     try {
         const verifiedTokenObj: JwtPayload = verifyAndDecode(req.header("Authorization"));
-        const userData: UserInterface = await User.findById(verifiedTokenObj.userId, "email name") // include '-_id' if we want to exclude _id from being retrieved
+        const data: UserInterface = await User.findById(verifiedTokenObj.userId, "email name") // include '-_id' if we want to exclude _id from being retrieved
+        const userLikes: number = await UserLikes.countDocuments({liked: verifiedTokenObj.userId})
+        let userData = {
+            name: data.name,
+            email: data.email,
+            likes: userLikes
+        }
         res.send({
             message: "User Information Data",
             user: userData
