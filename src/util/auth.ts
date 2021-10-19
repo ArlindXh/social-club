4687d10c905831cfbc142b96bf8f9c31b2ff17d8
ArlindXh <xhakoliarlind@gmail.com>
2021-10-19 02:39:00 +0200

    add validatePassword method for authentication

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -5,3 +5,8 @@ export const hashPassword = async (password: string) => {
 
     return await bcrypt.hash(password, salt);
 }
+
+export const validatePassword = async (password: string, hashedPassword: string) => {
+
+    return await bcrypt.compare(password, hashedPassword)
+}
