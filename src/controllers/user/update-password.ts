6d96378a3daad5469ee035bbf4509a88fda58a3c
ArlindXh <xhakoliarlind@gmail.com>
2021-10-19 02:49:17 +0200

    add functionality forupdate password

---
+++ src/controllers/user/update-password.ts
@@ -0,0 +1,36 @@
+import { RequestHandler } from 'express';
+import User, { UserInterface } from '../../models/Users';
+import { verifyAndDecode } from "../../util/jwt";
+import { JwtPayload } from "jsonwebtoken"
+import { updatePasswordValidation } from "../../util/validation";
+import { validatePassword, hashPassword } from "../../util/auth";
+import logger from '../../logger';
+
+const updatePassword: RequestHandler = async (req: any, res) => {
+    try {
+        const verifiedTokenObj: JwtPayload = verifyAndDecode(req.header("Authorization"));
+        let validation = updatePasswordValidation(req.body);
+        if (validation.error) {
+            let errorMessage = validation.error?.details[0]?.message;
+            return res.status(400).send(errorMessage)
+        }
+        const { oldPassword, newPassword } = req.body;
+        const data: UserInterface = await User.findById(verifiedTokenObj.userId);
+        const correctPassword = await validatePassword(oldPassword, data.password);
+        if (!correctPassword) return res.status(400).send({ message: "Incorrect password" });
+        
+        const hashedPassword = await hashPassword(newPassword);
+
+        await User.updateOne({ email: data.email }, { password: hashedPassword })
+
+        res.send({
+            message: "User password successfully updated",
+        });
+
+    } catch (error) {
+        logger.error(error);
+        res.status(500).send(error);
+    }
+};
+
+export default updatePassword;
