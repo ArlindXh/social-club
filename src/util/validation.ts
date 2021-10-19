d25a04c1e3990f2a6ff42a1f7d952c75e8009f5a
ArlindXh <xhakoliarlind@gmail.com>
2021-10-19 17:44:46 +0200

    add name validation to contain atleast 6 chars

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -3,7 +3,7 @@ import Joi from '@hapi/joi';
 
 export const registerValidation = (data: Object) => {
     const schema = Joi.object().keys({
-        name: Joi.string().required(),
+        name: Joi.string().min(6).required(),
         email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'de', 'io'] } }),
         password: Joi.string().min(6).required(),
         likes: Joi.number()
